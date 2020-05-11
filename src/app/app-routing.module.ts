e35f7d1fc8837d006fd8dbbb85fe291777ce111d
Tetiana Khotiaintseva <tetiana.khot@gmail.com>
2020-05-11 22:57:50 +0200

    Simple welcome section

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [
-  // { path: 'about', component: AboutComponent }
-];
+const routes: Routes = [{ path: '', component: WelcomeComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, { useHash: true })],
